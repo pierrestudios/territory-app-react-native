3ec5bcf59ef6e5adf1fede743bb287626e3407f0
Pierre Studios <info@pierrestudios.com>
2018-08-04 14:24:36 -0400

    Language settup

--- components/common/settings.js
+++ components/common/settings.js
@@ -1,8 +1,21 @@
+import * as languages from '../languages/index';
+
+// Site Prefs (Temp - will be stored in User Prefs)
+import prefs from '../../.user-prefs';
+
+// Site Settings
+const settings = {
+  ...prefs,
+  sitePath: prefs.lang, // Site URI
+  langPacks: {
+    [prefs.lang]: languages[prefs.lang]
+  }
+}
+
 export default (key) => {
-  const settings = {};
-  if (settings && key) 
+  if (!!settings && !!key) 
     return settings[key];
     
-  if (settings) 
+  if (!!settings) 
     return settings;
 }
