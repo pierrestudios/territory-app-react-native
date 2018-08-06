3b89cb2a40711a1a48e031c2f6f06ad233dd4d73
Pierre Studios <info@pierrestudios.com>
2018-08-05 20:48:03 -0400

    Enhance NavService

--- components/common/nav-service.js
+++ components/common/nav-service.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import { NavigationActions } from 'react-navigation';
-
 let _navigator;
-
-function setTopLevelNavigator(navigatorRef) {
-  console.log('setTopLevelNavigator()', navigatorRef);
-  _navigator = navigatorRef;
-}
 
 function setNavigator(navigator) {
   _navigator = navigator;
 }
 
 function navigate(routeName, params) {
-  // console.log('navigate()', {_navigator, routeName, params});
   _navigator.navigate(routeName, params);
 }
 
 export default {
   navigate,
-  setNavigator,
-  setTopLevelNavigator,
+  setNavigator
 };
