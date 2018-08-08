ae27ab67c8d33709297862a6735f07c5bd1eed19
Pierre Studios <info@pierrestudios.com>
2018-08-08 00:54:30 -0400

    Refactor Button component

--- components/elements/button.js
+++ components/elements/button.js
@@ -1,37 +1,22 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 import UTILS from '../common/utils';
 
-import styles, {colors} from '../styles/main';
-
-export class Link extends React.Component {
-  render() {
-    const buttonStyle = UTILS.getElementStyles(this.props, styles["main-menu-link"]);
-    if (!!this.props.disabled) {
-      buttonStyle.style.push([styles.disabled]);
-    }
-
-    return (
-      <TouchableOpacity
-        {...buttonStyle}
-        onPress={!!this.props.disabled ? () => console.log('disabled') : this.props.onPress}
-        >
-        <Text style={[styles["main-menu-button-text"], styles["text-color-blue"]]}> {this.props.children || this.props.title} </Text>
-      </TouchableOpacity>
-    )
-  }
+export const Link = (props) => {
+  return (
+    <Button {...props} baseStyle={styles["main-menu-link"]} />
+  )
 }
 
-const ButtonLink = (props) => {
-	// const classes = classNames(style['button-link'], {[style[props.class]]: props.class}, {[style.disabled]: props.disabled}, {[props.customClass]: props.customClass});
-	// return <Link {...props} class={classes}>{props.children}</Link>;
-};
-
-export {ButtonLink};
+export const ButtonLink = (props) => {
+	return (
+    <Button {...props} baseStyle={styles["button-link"]} textStyle={styles["button-link-text"]} />
+  )
+}; 
 
 export const Button = (props) => {
-  const buttonStyle = UTILS.getElementStyles(props, styles["main-menu-button"]);
+  const buttonStyle = UTILS.getElementStyles(props, (props.baseStyle || styles["main-menu-button"]));
   if (!!props.disabled) {
     buttonStyle.style.push([styles.disabled]);
   }
@@ -40,7 +25,7 @@ export const Button = (props) => {
       {...buttonStyle}
       onPress={!!props.disabled ? () => console.log('disabled') : props.onPress}
       >
-      <Text style={styles["main-menu-button-text"]}> {props.children || props.title} </Text>
+      <Text style={[styles["main-menu-button-text"], props.textStyle || null]}> {props.children || props.title} </Text>
     </TouchableOpacity>
   )
 }
