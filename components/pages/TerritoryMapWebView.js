cd4e63a6b891a7d1729c208515024acad0715d2b
Pierre Studios <info@pierrestudios.com>
2018-08-31 22:07:38 -0400

    Rework TerritoryMapWebView to load fn on callback

--- components/pages/TerritoryMapWebView.js
+++ components/pages/TerritoryMapWebView.js
@@ -23,20 +23,29 @@ export default class TerritoryMapWebView extends React.Component {
     // console.log('addressesStr', addressesStr);
     // console.log('boundariesStr', boundariesStr);
 
-    const jsScript = `MapFn.init('${apiKey}'); MapFn.initializeMap(${addressesStr}, ${boundariesStr});`;
-
+    // const jsScript = `MapFn.init('${apiKey}', function(){MapFn.initializeMap(${addressesStr}, ${boundariesStr})});`;
     // console.log('jsScript', jsScript);
-    
+
+    const jsScript2 = 'MapFn.initScript("https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&libraries=drawing,geometry&callback=MapFn.initGA", function(){MapFn.initializeMap(' + addressesStr + ', ' + boundariesStr + ')})';
+
     return (
       <WebView
+        // source={{html: testHtml}}
         source={html}
+        // ref={ref => (this.webview = ref)}
         geolocationEnabled={true}
-        // injectJavaScript={() => alert(1)}
-        injectedJavaScript={jsScript}
+        javaScriptEnabled={true}
+        // injecteJavaScript="alert(1)"
+        // injectedJavaScript="alert(1)"
+        // injectedJavaScript={jsScript}
+        injectedJavaScript={jsScript2}
         onError={() => console.log('onError')}
-        onLoadEnd={() => console.log('onLoadEnd')}
+        onLoadEnd={() => {
+          console.log('onLoadEnd');
+        }}
         onLoadStart={() => console.log('onLoadStart')}
         onLoad={() => console.log('onLoad')}
+        onMessage={(event) => console.log('onMessage', event.nativeEvent.data)} // event.nativeEvent.data
       />
     );
   }
