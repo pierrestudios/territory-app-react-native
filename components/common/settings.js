80cff4ede6c409f77541bb14383807da1c8be56a
Pierre Studios <info@pierrestudios.com>
2018-09-06 10:11:20 -0400

    Rework Splash page to wait for Settings to load before redirecting

--- components/common/settings.js
+++ components/common/settings.js
@@ -3,9 +3,8 @@ import * as languages from '../languages/index';
 
 // TODO: 
 // store GOOGLE_API_KEY in Data.user.GAKey after login success (Api needs to provide env vars)
-
-const DEFAULT_LANG = 'en';
 const GOOGLE_API_KEY = "AIzaSyDaLCZdxygUQAZB6_QxxbQouUDq-RuFzVM"; 
+const DEFAULT_LANG = 'en';
 
 let instance = null;
 
@@ -18,28 +17,20 @@ class UserData {
 		}
 
 		// Note: Need to load user for user settings
-    this.loadSavedUser()
-      .then(user => {
+		this.loadSavedUser();
+		
+		const prefs = {
+			"lang": DEFAULT_LANG
+		};
 
-				const prefs = !!user && {
-					"lang": user.lang,
-					"apiUrl": user.apiUrl,
-					"apiPath": user.apiPath,
-					"GAKey": GOOGLE_API_KEY 
-				} || {
-					"lang": DEFAULT_LANG
-				}
-
-				this._settings = {
-					...prefs,
-					languages,
-					defaultLang: DEFAULT_LANG,
-					langPacks: {
-						[prefs.lang]: languages[prefs.lang]
-					}
-				}
-				
-			});
+		this._settings = {
+			...prefs,
+			languages,
+			defaultLang: DEFAULT_LANG,
+			langPacks: {
+				[prefs.lang]: languages[prefs.lang]
+			}
+		}
 
 		return instance;
   }
@@ -51,13 +42,33 @@ class UserData {
 			const user = await AsyncStorage.getItem('user');
 			if (user) {
 				this._user = JSON.parse(user);
-				return this._user;
+				// return this._user;
+
+				const prefs = {
+					"lang": this._user.lang || DEFAULT_LANG,
+					"apiUrl": this._user.apiUrl,
+					"apiPath": this._user.apiPath,
+					"GAKey": GOOGLE_API_KEY 
+				};
+
+				this._settings = {
+					...this._settings,
+					...prefs,
+					langPacks: {
+						[prefs.lang]: languages[prefs.lang]
+					}
+				}
+
+				// console.log('this._settings:loadSavedUser', {lang: this._settings.lang, apiPath: this._settings.apiPath});
+					
 			}
 		} catch (error) {
 			UTILS.logError(error);
 		}
 	}
 	getSetting(key) {
+		console.log('getSetting:key', key);
+		
 		if (!!this._settings && !!key) 
 			return this._settings[key]
 
--- components/pages/Splash.js
+++ components/pages/Splash.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 
+import Data from '../common/data';
+import UTILS from '../common/utils';
 import NavigationService from '../common/nav-service';
-// import Data from '../common/data';
-// import UTILS from '../common/utils';
-import styles, {colors} from '../styles/main';
+import getSiteSetting from '../common/settings';
 
 export default class Splash extends React.Component {
   static navigationOptions = ({navigation}) => {
 		return {
       header: null,
-			headerTitle: null, // <Logo />,
+			headerTitle: null, 
 		}
   }
   componentWillMount() {
     NavigationService.setNavigator(this.props.navigation);
+  }
+  componentDidMount() {
+    this.props.navigation.addListener('willFocus', () => {
+      this.doRedirect();
+    });
+
+    this.doRedirect();
+  }
+  doRedirect() {
+    const state = this.state;
+		// console.log('Splash:doRedirect() state', state)
+
+    UTILS.waitForIt(() => !!getSiteSetting('defaultLang'), () => {
+      const user = Data.unAuthUser;
+      // console.log('user', user);
+
+      if (!user || !user.apiUrl) {
+        return NavigationService.navigate('UserPrefs')
+      }
+      
+      if ((!user || !user.userId || !user.token) && !!user && !!user.apiPath) {
+        return NavigationService.navigate('Login')
+      }
+
+      if (!!user && !!user.userId && !!user.token) {
+        return NavigationService.navigate('Home')
+      }
+    });
   }
   render() {
 		return (
