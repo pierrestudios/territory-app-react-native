298377cb41c85ebe2ed7dbcd5c9c88bc0453d4ea
Pierre Studios <info@pierrestudios.com>
2019-01-11 18:09:54 -0500

    Fix header for Home and Users on Android

--- components/pages/home.js
+++ components/pages/home.js
@@ -1,211 +1,278 @@
-import React from 'react';
-import { View, Text, ScrollView, SafeAreaView, StatusBar } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import { View, Text, ScrollView, SafeAreaView, StatusBar } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-import Logo from '../elements/Logo';
-import {Button, ButtonLink, ButtonHeader} from '../elements/Button';
-import Heading from '../elements/Heading';
-import Loading from '../elements/Loading';
-import Modal from '../elements/Modal';
+import Logo from "../elements/Logo";
+import { Button, ButtonLink, ButtonHeader } from "../elements/Button";
+import Heading from "../elements/Heading";
+import Loading from "../elements/Loading";
+import Modal from "../elements/Modal";
 
-import NavigationService from '../common/nav-service';
-import Language from '../common/lang';
-import Data from '../common/data';
-import UTILS from '../common/utils';
-import styles, {colors} from '../styles/main';
+import NavigationService from "../common/nav-service";
+import Language from "../common/lang";
+import Data from "../common/data";
+import UTILS from "../common/utils";
+import styles, { colors } from "../styles/main";
 
 export default class Home extends React.Component {
-  static navigationOptions = ({navigation}) => {
-		return {
-			headerTitle: <Logo />,
-			headerLeft: (<View />), // To center on Andriod
-			headerRight: (
-				<ButtonHeader
-					onPress={() => {navigation.setParams({
-						openUserInfo: !navigation.getParam('openUserInfo'), headerTriggered: true
-					})}}
-					title={<FontAwesome name="user-circle" size={20} color="#fff" />}
-					color="#fff"
-				/>
-			),
-		}
-	};
+  static navigationOptions = ({ navigation }) => {
+    return {
+      ...UTILS.headerNavOptionsDefault,
+      headerTitle: <Logo />,
+      headerLeft: <View />, // To center on Andriod
+      headerRight: (
+        <ButtonHeader
+          onPress={() => {
+            navigation.setParams({
+              openUserInfo: !navigation.getParam("openUserInfo"),
+              headerTriggered: true
+            });
+          }}
+          title={<FontAwesome name="user-circle" size={20} color="#fff" />}
+          color="#fff"
+        />
+      )
+    };
+  };
   state = {
-		user: null,
-		drawerOpened: false,
-		modalVisible: false
-	}; 
-	componentWillMount() {
-		NavigationService.setNavigator(this.props.navigation);
+    user: null,
+    drawerOpened: false,
+    modalVisible: false
+  };
+  componentWillMount() {
+    NavigationService.setNavigator(this.props.navigation);
 
-		const user = Data.user;
+    const user = Data.user;
 
-		if (!user || !user.token) {
-			Data.loadSavedUser()
-				.then(user => {
-					this.setState({user});
-				})
-				.catch(UTILS.logError)
-		} else {
-			this.setState({user});
-		}
-	}
-	componentDidMount() {
-		this.props.navigation.addListener('willFocus', () => {
-			// console.log('willFocus:state.user', this.state.user)
-			// console.log('willFocus:Data.user', Data.user)
-			if (!this.state.user || !this.state.user.token)
-				this.setState({user: Data.user});
-		});
-	}
-	componentWillReceiveProps(props) {
-		// console.log('props', props);
-		if (props.navigation && !!props.navigation.getParam('headerTriggered')) {
-			this.setModalVisible(props.navigation.getParam('openUserInfo'))
-		}
-	}
+    if (!user || !user.token) {
+      Data.loadSavedUser()
+        .then(user => {
+          this.setState({ user });
+        })
+        .catch(UTILS.logError);
+    } else {
+      this.setState({ user });
+    }
+  }
+  componentDidMount() {
+    this.props.navigation.addListener("willFocus", () => {
+      // console.log('willFocus:state.user', this.state.user)
+      // console.log('willFocus:Data.user', Data.user)
+      if (!this.state.user || !this.state.user.token)
+        this.setState({ user: Data.user });
+    });
+  }
+  componentWillReceiveProps(props) {
+    // console.log('props', props);
+    if (props.navigation && !!props.navigation.getParam("headerTriggered")) {
+      this.setModalVisible(props.navigation.getParam("openUserInfo"));
+    }
+  }
   setModalVisible(visible) {
-    this.setState({
-			modalVisible: visible
-		}, () => this.props.navigation.setParams({
-			openUserInfo: visible, headerTriggered: false
-		})) // Bug: causes crash
+    this.setState(
+      {
+        modalVisible: visible
+      },
+      () =>
+        this.props.navigation.setParams({
+          openUserInfo: visible,
+          headerTriggered: false
+        })
+    ); // Bug: causes crash
   }
   goToPage(path) {
-    NavigationService.navigate(path)
-	}
-	sendLogout() {
-		this.setState({
-			user: null,
-			modalVisible: false
-		}, () => {
-			Data.reLogin();
-		});
-	}
-	render() {
-		// console.log('Home:render:state', this.state);
-		if (!this.state.user) return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Loading />
-      </View> 
-		)
+    NavigationService.navigate(path);
+  }
+  sendLogout() {
+    this.setState(
+      {
+        user: null,
+        modalVisible: false
+      },
+      () => {
+        Data.reLogin();
+      }
+    );
+  }
+  render() {
+    // console.log('Home:render:state', this.state);
+    if (!this.state.user)
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Loading />
+        </View>
+      );
 
-		return ( 
-			<SafeAreaView style={[styles.container, { backgroundColor: '#ecf0f1' }]}>
-        <StatusBar
-          barStyle="light-content"
-				/>
+    return (
+      <SafeAreaView style={[styles.container, { backgroundColor: "#ecf0f1" }]}>
+        <StatusBar barStyle="light-content" />
 
-				{/** Note: WebView cannot handle PDF download **/}
-				{/* 
+        {/** Note: WebView cannot handle PDF download **/}
+        {/* 
 					<Button 
 						disabled key="s-13"	
 						onPress={() => UTILS.openFrameUrl('s-13')} 
 						title={Language.translate('Print_S-13', 'Print S-13 Form')} 
 						/>, 
 				*/}
-				
-				<ScrollView contentContainerStyle={styles["scroll-view"]} key="home-page">
-					{this.state.user && this.state.user.isNoteEditor ?
-						[
-						<Heading key="territory-heading">
-							{Language.translate('Manage Your Congregation Territory')}
-						</Heading>,
-						<View key="menu-nav" style={styles["main-menu"]}>
-							{this.state.user.isAdmin 
-								? <Button 
-									key="users" 
-									onPress={() => this.goToPage("Users")} 
-									title={Language.translate('Users')} 
-									/> 
-								: null
-							}
-							{this.state.user.isManager ? [
-								<Button 
-									key="publishers" 
-									onPress={() => this.goToPage("Publishers")} 
-									title={Language.translate('Publishers')} 
-									/>,
-								<Button 
-									key="boundaries" 
-									onPress={() => NavigationService.navigate('WebViewApi', {
-										url: 'boundaries', title: Language.translate('Territory Map')
-									})} 
-									title={Language.translate('Territory Map')} 
-									/>,
-								<Button 
-									key="territories-all" 
-									onPress={() => this.goToPage("TerritoriesAll")} 
-									title={Language.translate('All Territories')} 
-									/>
-							]: null}
-							<Button 
-								key="territories" 
-								onPress={() => this.goToPage("Territories")} 
-								title={Language.translate('My Territories')} 
-								/> 
-						</View>
-						] : [
-							<Heading key="no-territories" >{Language.translate('No territories')}</Heading>,
-							<View key="menu-nav" style={[styles["main-menu"], {}]}>
-								<Text textStyle={{fontSize: 16}}>
-									{Language.translate('You don\'t have privilege to manage territories')}
-								</Text>
-							</View>
-						]
-					}
-				</ScrollView>
-				
-				<Modal
-					animationType="fade"
-					visible={this.state.modalVisible}
-					onCloseModal={() => {
-						this.setModalVisible(!this.state.modalVisible);
-					}}
-					>
-					<View style={styles["modal-view"]}>
-						<Heading textStyle={{marginBottom: 20, marginTop: 0}}>{Language.translate('My User Account')}</Heading>
 
-						<ButtonLink 
-							customStyle={[styles["heading-button-link"], {
-								width: 100, alignSelf: 'center', padding: 15, marginBottom: 10, borderColor: colors["grey-lite"], borderWidth: 1
-							}]}
-							onPress={() => NavigationService.navigate('WebViewExternal', {
-								url: 'http://www.territory-app.net/', title: Language.translate('More Information')
-							})}>{Language.translate('About')} 
-						</ButtonLink>
-					
-						<ButtonLink
-							customStyle={[styles["heading-button-link"], {
-								width: 100, alignSelf: 'center', padding: 15, marginBottom: 10, borderColor: colors["grey-lite"], borderWidth: 1
-							}]}
-							onPress={() => {
-								NavigationService.navigate('UserPrefs');
-							}}>{Language.translate('Settings')} 
-						</ButtonLink>
+        <ScrollView
+          contentContainerStyle={styles["scroll-view"]}
+          key="home-page"
+        >
+          {this.state.user && this.state.user.isNoteEditor
+            ? [
+                <Heading key="territory-heading">
+                  {Language.translate("Manage Your Congregation Territory")}
+                </Heading>,
+                <View key="menu-nav" style={styles["main-menu"]}>
+                  {this.state.user.isAdmin ? (
+                    <Button
+                      key="users"
+                      onPress={() => this.goToPage("Users")}
+                      title={Language.translate("Users")}
+                    />
+                  ) : null}
+                  {this.state.user.isManager
+                    ? [
+                        <Button
+                          key="publishers"
+                          onPress={() => this.goToPage("Publishers")}
+                          title={Language.translate("Publishers")}
+                        />,
+                        <Button
+                          key="boundaries"
+                          onPress={() =>
+                            NavigationService.navigate("WebViewApi", {
+                              url: "boundaries",
+                              title: Language.translate("Territory Map")
+                            })
+                          }
+                          title={Language.translate("Territory Map")}
+                        />,
+                        <Button
+                          key="territories-all"
+                          onPress={() => this.goToPage("TerritoriesAll")}
+                          title={Language.translate("All Territories")}
+                        />
+                      ]
+                    : null}
+                  <Button
+                    key="territories"
+                    onPress={() => this.goToPage("Territories")}
+                    title={Language.translate("My Territories")}
+                  />
+                </View>
+              ]
+            : [
+                <Heading key="no-territories">
+                  {Language.translate("No territories")}
+                </Heading>,
+                <View key="menu-nav" style={[styles["main-menu"], {}]}>
+                  <Text textStyle={{ fontSize: 16 }}>
+                    {Language.translate(
+                      "You don't have privilege to manage territories"
+                    )}
+                  </Text>
+                </View>
+              ]}
+        </ScrollView>
 
-						<ButtonLink
-							customStyle={[styles["heading-button-link"], {
-								width: 100, alignSelf: 'center', padding: 15, marginBottom: 10, borderColor: colors["grey-lite"], borderWidth: 1
-							}]}
-							onPress={() => {
-								NavigationService.navigate('Login');
-							}}>{Language.translate('Account')} 
-						</ButtonLink>
+        <Modal
+          animationType="fade"
+          visible={this.state.modalVisible}
+          onCloseModal={() => {
+            this.setModalVisible(!this.state.modalVisible);
+          }}
+        >
+          <View style={styles["modal-view"]}>
+            <Heading textStyle={{ marginBottom: 20, marginTop: 0 }}>
+              {Language.translate("My User Account")}
+            </Heading>
 
-						<ButtonLink
-							customStyle={[styles["heading-button-link"], {
-								width: 100, alignSelf: 'center', padding: 15, marginBottom: 10, borderColor: colors["grey-lite"], borderWidth: 1
-							}]}
-							onPress={() => {
-								this.sendLogout();
-							}}>{Language.translate('Sign Out')} 
-						</ButtonLink>
+            <ButtonLink
+              customStyle={[
+                styles["heading-button-link"],
+                {
+                  width: 100,
+                  alignSelf: "center",
+                  padding: 15,
+                  marginBottom: 10,
+                  borderColor: colors["grey-lite"],
+                  borderWidth: 1
+                }
+              ]}
+              onPress={() =>
+                NavigationService.navigate("WebViewExternal", {
+                  url: "http://www.territory-app.net/",
+                  title: Language.translate("More Information")
+                })
+              }
+            >
+              {Language.translate("About")}
+            </ButtonLink>
 
-					</View>
-				</Modal>
+            <ButtonLink
+              customStyle={[
+                styles["heading-button-link"],
+                {
+                  width: 100,
+                  alignSelf: "center",
+                  padding: 15,
+                  marginBottom: 10,
+                  borderColor: colors["grey-lite"],
+                  borderWidth: 1
+                }
+              ]}
+              onPress={() => {
+                NavigationService.navigate("UserPrefs");
+              }}
+            >
+              {Language.translate("Settings")}
+            </ButtonLink>
 
-			</SafeAreaView>
-		)
-	}
+            <ButtonLink
+              customStyle={[
+                styles["heading-button-link"],
+                {
+                  width: 100,
+                  alignSelf: "center",
+                  padding: 15,
+                  marginBottom: 10,
+                  borderColor: colors["grey-lite"],
+                  borderWidth: 1
+                }
+              ]}
+              onPress={() => {
+                NavigationService.navigate("Login");
+              }}
+            >
+              {Language.translate("Account")}
+            </ButtonLink>
+
+            <ButtonLink
+              customStyle={[
+                styles["heading-button-link"],
+                {
+                  width: 100,
+                  alignSelf: "center",
+                  padding: 15,
+                  marginBottom: 10,
+                  borderColor: colors["grey-lite"],
+                  borderWidth: 1
+                }
+              ]}
+              onPress={() => {
+                this.sendLogout();
+              }}
+            >
+              {Language.translate("Sign Out")}
+            </ButtonLink>
+          </View>
+        </Modal>
+      </SafeAreaView>
+    );
+  }
 }
--- components/pages/users.js
+++ components/pages/users.js
@@ -1,125 +1,175 @@
-import React from 'react';
-import { Text, View, FlatList, TouchableOpacity } from 'react-native';
-import Swipeout from 'react-native-swipeout';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import Swipeout from "react-native-swipeout";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import Data from '../common/data';
-import Language from '../common/lang';
-import UTILS from '../common/utils';
-import NavigationService from '../common/nav-service';
+import Data from "../common/data";
+import Language from "../common/lang";
+import UTILS from "../common/utils";
+import NavigationService from "../common/nav-service";
 
-import Heading from '../elements/Heading';
-import Loading from '../elements/Loading'; 
+import Heading from "../elements/Heading";
+import Loading from "../elements/Loading";
 
-import styles, { colors } from '../styles/main';
+import styles, { colors } from "../styles/main";
 
 export default class Users extends React.Component {
-  static navigationOptions = {
-    headerTitle: null,
-    title: 'Territory Users',
-    headerTintColor: '#fff',
+  static navigationOptions = ({ navigation }) => {
+    return {
+      ...UTILS.headerNavOptionsDefault,
+      headerRight: <View />, // To center on Andriod
+      headerTitle: null,
+      title: "Territory Users",
+      headerTintColor: "#fff"
+    };
   };
-	componentWillMount() {
-		if (!Data.user) return;
-   
-		Data.getApiData('users')
-			.then(users => {
-				this.setState({users}, () => {
-					users && Data.getApiData('publishers/filter', {"userId": null}, 'POST')
-						.then(publishers => {
-							this.setState({unattachedPublishers: publishers}); 
-						});	
-				}); 
-			});
+  componentWillMount() {
+    if (!Data.user) return;
 
-	}
-	
-	render() {
+    Data.getApiData("users").then(users => {
+      this.setState({ users }, () => {
+        users &&
+          Data.getApiData("publishers/filter", { userId: null }, "POST").then(
+            publishers => {
+              this.setState({ unattachedPublishers: publishers });
+            }
+          );
+      });
+    });
+  }
+
+  render() {
     // console.log('render:props', props)
     const state = this.state || {};
-		// console.log('state', state)
+    // console.log('state', state)
 
-		if (!state.users)
-			return <Loading />;
+    if (!state.users) return <Loading />;
 
-		const listings = state.users.length 
-			? this.getListings(state.users) 
-			: <Text>{Language.translate('There are no users')}</Text>;
-		
-		return (
-			<View style={[styles.section, styles.content]}>
-				<Heading>{Language.translate('Users')}</Heading>
-				<View style={[styles.section, styles['users-results']]}>
-					{listings}
-				</View>
-			</View>
-		);
-	}
-	getListings(data = []) {
-		return (
-			<FlatList
-				contentContainerStyle={styles.listings}
-				data={data.sort(UTILS.sortUser)}
-				keyExtractor={(item) => item.userId.toString()}
-				renderItem={({item}) => (
-					<Swipeout key={item.userId} right={[
-						{ text: Language.translate('Details'), type: 'primary', onPress: () => this.viewDetails(item) },
-            { text: Language.translate('Edit'), backgroundColor: colors.green, onPress: () => this.editUser(item) }, 
-            { text: Language.translate('Delete'), type: 'delete', onPress: () => this.notifyDelete(item, state.user) } // backgroundColor: colors.red  
-					]} autoClose={true} close={true}>
-					<View style={[styles['listings-item']]}>
-						<TouchableOpacity 
-							style={[styles['listings-name'], styles['user-listings-name'], {width: '95%'}]} 
-							onPress={() => this.viewDetails(item)}
-							>
-							<View style={[styles['listings-name'], {marginTop: 5, left: 5, width: '50%'}]}>
-								{item.publisher ? 
-								<Text numberOfLines={1} style={[styles['listings-name-text'], {fontWeight: 'bold', color: colors.black}]}>
-									{(item.publisher.firstName + ' ' + item.publisher.lastName)}
-								</Text>
-								: 
-								<Text numberOfLines={1} style={[styles['listings-name-text'], {color: colors["grey-lite"]}]}>
-									{Language.translate('No Publisher')}
-								</Text>
-								}
-							</View>
-							<View style={[styles['listings-email'], {marginTop: -3} ]}>
-								<Text numberOfLines={1} style={styles['listings-type-text']}>
-									{UTILS.userTypeLabel(item.userType)}
-								</Text>
-								<Text numberOfLines={1} style={styles['listings-email-text']}>{item.email}</Text>
-							</View>	
-						</TouchableOpacity>
-						<View style={[styles["listings-right-arrow"]]}>
-							<Ionicons name="ios-arrow-forward" size={24} color={colors["grey-lite"]} />
-						</View>
-					</View>
-					</Swipeout>
-				)}
-			/>
-		);
-	}
-	viewDetails(data) {
-    NavigationService.navigate('UserDetails', {
-			data, 
-			unattachedPublishers: this.state.unattachedPublishers, 
-			updateUsers: this.updateUsers
-		})
-	}
-	removeUser = (userId) => {
-		this.setState({
-			publishers: this.state.publishers.filter(p => p.userId !== userId)
-		})
-	}
-	updateUsers = (updatedUser, unattachedPublishers = null) => {
-		this.setState({
-      unattachedPublishers: unattachedPublishers ? unattachedPublishers.sort(UTILS.sortPublisher) : this.state.unattachedPublishers,
-			users: this.state.users.map(p => {
-				if (p.userId === updatedUser.userId) {
-					return updatedUser;
-				}
-				return p;
-			})
-		})
-	}
+    const listings = state.users.length ? (
+      this.getListings(state.users)
+    ) : (
+      <Text>{Language.translate("There are no users")}</Text>
+    );
+
+    return (
+      <View style={[styles.section, styles.content]}>
+        <View style={[styles.section, styles["users-results"]]}>
+          {listings}
+        </View>
+      </View>
+    );
+  }
+  getListings(data = []) {
+    return (
+      <FlatList
+        contentContainerStyle={styles.listings}
+        data={data.sort(UTILS.sortUser)}
+        keyExtractor={item => item.userId.toString()}
+        renderItem={({ item }) => (
+          <Swipeout
+            key={item.userId}
+            right={[
+              {
+                text: Language.translate("Details"),
+                type: "primary",
+                onPress: () => this.viewDetails(item)
+              },
+              {
+                text: Language.translate("Edit"),
+                backgroundColor: colors.green,
+                onPress: () => this.editUser(item)
+              },
+              {
+                text: Language.translate("Delete"),
+                type: "delete",
+                onPress: () => this.notifyDelete(item, state.user)
+              } // backgroundColor: colors.red
+            ]}
+            autoClose={true}
+            close={true}
+          >
+            <View style={[styles["listings-item"]]}>
+              <TouchableOpacity
+                style={[
+                  styles["listings-name"],
+                  styles["user-listings-name"],
+                  { width: "95%" }
+                ]}
+                onPress={() => this.viewDetails(item)}
+              >
+                <View
+                  style={[
+                    styles["listings-name"],
+                    { marginTop: 5, left: 5, width: "50%" }
+                  ]}
+                >
+                  {item.publisher ? (
+                    <Text
+                      numberOfLines={1}
+                      style={[
+                        styles["listings-name-text"],
+                        { fontWeight: "bold", color: colors.black }
+                      ]}
+                    >
+                      {item.publisher.firstName + " " + item.publisher.lastName}
+                    </Text>
+                  ) : (
+                    <Text
+                      numberOfLines={1}
+                      style={[
+                        styles["listings-name-text"],
+                        { color: colors["grey-lite"] }
+                      ]}
+                    >
+                      {Language.translate("No Publisher")}
+                    </Text>
+                  )}
+                </View>
+                <View style={[styles["listings-email"], { marginTop: -3 }]}>
+                  <Text numberOfLines={1} style={styles["listings-type-text"]}>
+                    {UTILS.userTypeLabel(item.userType)}
+                  </Text>
+                  <Text numberOfLines={1} style={styles["listings-email-text"]}>
+                    {item.email}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <View style={[styles["listings-right-arrow"]]}>
+                <Ionicons
+                  name="ios-arrow-forward"
+                  size={24}
+                  color={colors["grey-lite"]}
+                />
+              </View>
+            </View>
+          </Swipeout>
+        )}
+      />
+    );
+  }
+  viewDetails(data) {
+    NavigationService.navigate("UserDetails", {
+      data,
+      unattachedPublishers: this.state.unattachedPublishers,
+      updateUsers: this.updateUsers
+    });
+  }
+  removeUser = userId => {
+    this.setState({
+      publishers: this.state.publishers.filter(p => p.userId !== userId)
+    });
+  };
+  updateUsers = (updatedUser, unattachedPublishers = null) => {
+    this.setState({
+      unattachedPublishers: unattachedPublishers
+        ? unattachedPublishers.sort(UTILS.sortPublisher)
+        : this.state.unattachedPublishers,
+      users: this.state.users.map(p => {
+        if (p.userId === updatedUser.userId) {
+          return updatedUser;
+        }
+        return p;
+      })
+    });
+  };
 }
