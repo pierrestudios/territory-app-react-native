a7cc07f5ef7f927b655777cf57d70c77d379a057
Pierre Studios <info@pierrestudios.com>
2019-03-02 21:30:04 -0500

    Fixed translations

--- components/pages/MyTerritories.js
+++ components/pages/MyTerritories.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { View } from 'react-native';
-import Territories from '../pages/Territories';
-import Language from '../common/lang';
-import UTILS from '../common/utils';
+import React from "react";
+import { View } from "react-native";
+import Territories from "../pages/Territories";
+import Language from "../common/lang";
+import UTILS from "../common/utils";
 
 export default class MyTerritories extends Territories {
-  static navigationOptions = {
-    ...UTILS.headerNavOptionsDefault,
-		headerRight: (<View />), // To center on Andriod
-    title: Language.translate('My Territories'),
-  }
-  allTerritories=false
+  static navigationOptions = ({ navigation }) => {
+    return {
+      ...UTILS.headerNavOptionsDefault,
+      headerRight: <View />, // To center on Andriod
+      title: Language.translate("My Territories")
+    };
+  };
+  allTerritories = false;
 }
