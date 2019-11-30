6092813eb6ec31c24b8786764dbd3e3767bcfdd5
pierrestudios <info@pierrestudios.com>
2019-11-29 23:06:37 -0500

    Fixed App versioning

--- src/components/version.js
+++ src/components/version.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { View, Text, Platform } from "react-native";
 
-import { expo } from "../../app.json";
+import appVersion from "../../app-version.json";
 import { version as packageVersion } from "../../package.json";
 
 export default () => {
   return (
     <View style={{ marginBottom: 10, marginTop: 10 }}>
       <Text>
-        VER: {expo ? expo[`${Platform.OS}-version`] : ""}
+        VER: {appVersion ? appVersion[`${Platform.OS}-version`] : ""}
         {packageVersion ? `-DEV-${packageVersion}` : ""}
       </Text>
     </View>
