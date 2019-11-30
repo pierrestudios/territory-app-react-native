b53d6bcdbddbf5f45a9c70ffefbf10dbac4ec14c
pierrestudios <info@pierrestudios.com>
2019-11-29 22:29:52 -0500

    Adjust version for each platform

--- src/components/version.js
+++ src/components/version.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 
 import { expo } from "../../app.json";
 import { version as packageVersion } from "../../package.json";
@@ -8,7 +8,8 @@ export default () => {
   return (
     <View style={{ marginBottom: 10, marginTop: 10 }}>
       <Text>
-        VER: {expo ? expo.version : ""}-{packageVersion}
+        VER: {expo ? expo[`${Platform.OS}-version`] : ""}
+        {packageVersion ? `-DEV-${packageVersion}` : ""}
       </Text>
     </View>
   );
